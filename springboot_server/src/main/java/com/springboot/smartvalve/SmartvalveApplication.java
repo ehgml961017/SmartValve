972dacf3013d0cde35c3870722a31452444631c2
SeungwanKu <atcc02019@gmail.com>
2020-07-16 19:41:20 +0900

    스프링부트 빌드

---
+++ springboot_server/src/main/java/com/springboot/smartvalve/SmartvalveApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.smartvalve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartvalveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartvalveApplication.class, args);
+    }
+
+}
