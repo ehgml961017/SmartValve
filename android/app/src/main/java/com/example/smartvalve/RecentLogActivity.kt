b68b17cdd2c8e06b296b8e17d14c92c600ce30b9
dohee <ehgml961017@gmail.com>
2020-07-18 18:16:51 +0900

    update log activity & add LogVO

--- android/app/src/main/java/com/example/smartvalve/RecentLogActivity.kt
+++ android/app/src/main/java/com/example/smartvalve/RecentLogActivity.kt
@@ -2,13 +2,9 @@ package com.example.smartvalve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_recent_log.*
 
-//private lateinit var listView: ListView
 class RecentLogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +17,18 @@ class RecentLogActivity : AppCompatActivity() {
             ConnectionDB()
         }.start()
 
-        var listItem = arrayListOf<String>()
+        var listItem:ArrayList<LogVO> = arrayListOf()
+        var log1 = LogVO(1, "닫힘", "닫힘", "2020/07/14\n14:37", "2020/07/14\n15:00")
+        listItem.add(log1)
         val header:View = layoutInflater.inflate(R.layout.layout_list_header,null)
 
         log_list.addHeaderView(header)
-        log_list.adapter = ArrayAdapter(this, R.layout.layout_list, listItem)
+        log_list.adapter = LogAdapter(this, R.layout.layout_list, listItem)
     }
 
     fun ConnectionDB(){
 
     }
+
+
 }
