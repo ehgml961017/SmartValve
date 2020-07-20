b0a3fe1836ddbd1ab45902ffe8b291ddae67287c
SeungwanKu <atcc02019@gmail.com>
2020-07-20 17:15:22 +0900

    jsp 수정, db수정

--- springboot_server/src/main/java/com/springboot/smartvalve/mapper/SvMapper.java
+++ springboot_server/src/main/java/com/springboot/smartvalve/mapper/SvMapper.java
@@ -7,5 +7,9 @@ import java.util.List;
 public interface SvMapper {
     public List<SvDTO> getValue() throws Exception;
     public void insertValue(SvDTO svDTO) throws Exception;
+    public void sw1_on(SvDTO svDTO) throws Exception;
+    public void sw1_off(SvDTO svDTO) throws Exception;
+    public void sw2_on(SvDTO svDTO) throws Exception;
+    public void sw2_off(SvDTO svDTO) throws Exception;
 
 }
--- springboot_server/src/main/java/com/springboot/smartvalve/service/SvService.java
+++ springboot_server/src/main/java/com/springboot/smartvalve/service/SvService.java
@@ -17,7 +17,22 @@ public class SvService {
         return svMapper.getValue();
     }
 
-    public void insertValue(SvDTO dto) throws Exception {
-        svMapper.insertValue(dto);
+    public void insertValue(SvDTO svDTO) throws Exception {
+        // sw 1 sw2가 둘다 0일때만 인서트 되게
+        svMapper.insertValue(svDTO);
+    }
+
+    public void onSw1(SvDTO svDTO) throws Exception {
+        svMapper.sw1_on(svDTO);
+    }
+    public void offSw1(SvDTO svDTO) throws Exception {
+        svMapper.sw1_off(svDTO);
+    }
+
+    public void onSw2(SvDTO svDTO) throws Exception {
+        svMapper.sw2_on(svDTO);
+    }
+    public void offSw2(SvDTO svDTO) throws Exception {
+        svMapper.sw2_off(svDTO);
     }
 }
