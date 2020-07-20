1d1bc292c0845b40c33d2d45535f669ccbe609a6
SeungwanKu <atcc02019@gmail.com>
2020-07-20 12:20:20 +0900

    테이블 뷰 생성 .

--- springboot_server/src/main/java/com/springboot/smartvalve/controller/testController.java
+++ springboot_server/src/main/java/com/springboot/smartvalve/controller/testController.java
@@ -1,14 +1,24 @@
 package com.springboot.smartvalve.controller;
 
+import com.springboot.smartvalve.dto.SvDTO;
+import com.springboot.smartvalve.service.SvService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
 public class testController {
-    @GetMapping("/")
-    public String test() {
-        System.out.println("test");
-        return "test";
+
+    @Autowired
+    SvService svService;
+    @GetMapping("/svList")
+    public void getList(Model model) throws Exception {
+        List<SvDTO> list = null;
+        list = svService.getValue();
+        model.addAttribute("list", list);
     }
 }
