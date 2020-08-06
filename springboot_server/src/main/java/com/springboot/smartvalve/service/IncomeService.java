8e1271ffe585ca2bc7afe394c373f6e8c82929ba
홍원기 <vnvn2778@gmail.com>
2020-08-06 17:15:38 +0900

    차트 완성!

---
+++ springboot_server/src/main/java/com/springboot/smartvalve/service/IncomeService.java
@@ -0,0 +1,10 @@
+package com.springboot.smartvalve.service;
+
+import com.springboot.smartvalve.dto.IncomeVO;
+
+import java.util.List;
+
+public interface IncomeService {
+
+    public List<IncomeVO> getIncome();
+}
